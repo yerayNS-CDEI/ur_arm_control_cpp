b9268280d0763227b4ddae349d797a019b824740
Yeray Navarro <yeray.navarro@estudiantat.upc.edu>
2025-07-17 11:38:56 +0200

    Now the orientations given by the sensors are with respect to the end effector frame, and later transformed to the global base frame. 17-07

--- scripts/sensors_orientation.py
+++ scripts/sensors_orientation.py
@@ -41,7 +41,7 @@ class SensorsOrientation(Node):
         self.create_subscription(JointState, "/joint_states", self.joint_state_callback, 10)
         self.create_subscription(Float32MultiArray, "/distance_sensors", self.listener_distance_callback, 10)
 
-        self.timer = self.create_timer(3.0, self.timer_callback)
+        # self.timer = self.create_timer(3.0, self.timer_callback)
 
     def end_effector_pose_callback(self, msg):
         self.end_effector_pose = msg
@@ -65,8 +65,6 @@ class SensorsOrientation(Node):
             )
         else:
             self.get_logger().warn("Message recevied was incomplete.")
-
-    def timer_callback(self):
 
         if not self.end_effector_pose:
             self.get_logger().info("No end effector pose retrieved yet")
@@ -98,35 +96,40 @@ class SensorsOrientation(Node):
         self.get_logger().info(f"Distance to wall from center: {distance:.2f} cm")
 
         # Pitch and yaw error angles 
-        yaw = np.arctan2(nw[0],nw[2])
-        pitch = np.arctan2(nw[1],nw[2])
+        pitch = -np.arctan2(nw[0],nw[2])
+        yaw = -np.arctan2(nw[1],nw[2])
         yaw_deg = np.rad2deg(yaw)
         pitch_deg = np.rad2deg(pitch)
 
         self.get_logger().info(f"Pitch: {pitch_deg:.2f} degrees")
         self.get_logger().info(f"Yaw: {yaw_deg:.2f} degrees")
 
-        rot = self.end_effector_pose.orientation
-        euler = R.from_quat([rot.x, rot.y, rot.z, rot.w]).as_euler('xyz')
-        self.get_logger().info(f"Current Orientation (rpy): roll={euler[0]:.2f}, pitch={euler[1]:.2f}, yaw={euler[2]:.2f}")
-        goal_orn = [euler[0], euler[1] + pitch, euler[2] + yaw]
-        self.get_logger().info(f"Demanded Orientation (rpy): roll={goal_orn[0]:.2f}, pitch={goal_orn[1]:.2f}, yaw={goal_orn[2]:.2f}")
-        goal_orn = R.from_euler('xyz', goal_orn, degrees=False)
-        q = goal_orn.as_quat()
+        # rot = self.end_effector_pose.orientation
+        # euler = R.from_quat([rot.x, rot.y, rot.z, rot.w]).as_euler('xyz')
+        # self.get_logger().info(f"Current Orientation (rpy): roll={euler[0]:.2f}, pitch={euler[1]:.2f}, yaw={euler[2]:.2f}")
+        # goal_orn = [euler[0], euler[1] + pitch, euler[2] + yaw]
+        # self.get_logger().info(f"Demanded Orientation (rpy): roll={goal_orn[0]:.2f}, pitch={goal_orn[1]:.2f}, yaw={goal_orn[2]:.2f}")
+        # goal_orn = R.from_euler('xyz', goal_orn, degrees=False)
+        # q = goal_orn.as_quat()
 
         # Convert orientation quaternion to rotation matrix
         rot = self.end_effector_pose.orientation
-        r = R.from_quat([rot.x, rot.y, rot.z, rot.w])
-        nw_global = r.apply(nw)
-        q = r.apply([0, pitch, yaw])
-        q = R.from_euler('xyz', q, degrees=False)
-        q = q.as_quat()
+        curr_orn_rot = R.from_quat([rot.x, rot.y, rot.z, rot.w])
+        curr_orn = R.from_quat([rot.x, rot.y, rot.z, rot.w]).as_euler('xyz')
+        self.get_logger().info(f"Current Orientation (rpy): roll={curr_orn[0]:.2f}, pitch={curr_orn[1]:.2f}, yaw={curr_orn[2]:.2f}")
+        q = curr_orn_rot.apply([0, pitch, yaw])
+        goal_orn = [curr_orn[0] + q[0], curr_orn[1] + q[1], curr_orn[2] + q[2]]
+        self.get_logger().info(f"Demanded Orientation (rpy): roll={goal_orn[0]:.2f}, pitch={goal_orn[1]:.2f}, yaw={goal_orn[2]:.2f}")
+        goal_orn = R.from_euler('xyz', goal_orn, degrees=False)
+        self.get_logger().info(f"Goal orientation matrix: {goal_orn.as_matrix()}")
+        q = goal_orn.as_quat()
 
         # Current end effector position
         pos = self.end_effector_pose.position
         p_current = np.array([pos.x, pos.y, pos.z])
 
         # Compute corrected position
+        nw_global = curr_orn_rot.apply(nw)
         self.get_logger().warn(f"Toggle Value: {self.toggle}")
         p_new = p_current - 0*(self.toggle)*abs(distance - self.ideal_distance) * nw_global / 100     # in meters!!
         self.toggle *= -1
@@ -141,7 +144,7 @@ class SensorsOrientation(Node):
         if np.any(np.isnan(joint_values)):
             self.get_logger().error("IK solution contains NaN. Aborting.")
             return
-
+        joint_values[5] = 0.0   # NEEDS TO BE MODIFIED IN CASE ANOTHER INITIAL SENSORS POSITION IS USED!!!
         # Publish GoalPose
         traj_msg = JointTrajectory()
         traj_msg.joint_names = [
@@ -169,6 +172,110 @@ class SensorsOrientation(Node):
         # goal_pose.orientation.z = q[2]
         # goal_pose.orientation.w = q[3]
         # self.publisher_goal_pose.publish(goal_pose)
+
+    # def timer_callback(self):
+
+    #     if not self.end_effector_pose:
+    #         self.get_logger().info("No end effector pose retrieved yet")
+    #         return
+
+    #     # normal vector to plate
+    #     nV = np.array([0.0, 0.0, 1.0])
+
+    #     # Wall intersection points
+    #     wA = self.pA + self.dA * nV
+    #     wB = self.pB + self.dB * nV
+    #     wC = self.pC + self.dC * nV
+    #     self.get_logger().info(f"Projected points: {wA}, {wB}, {wC}")
+
+    #     # Plane vectors
+    #     v1= wB - wA
+    #     v2= wC - wA
+
+    #     #Wall normal
+    #     nW0= np.cross(v1,v2)
+    #     self.get_logger().info(f"Wall normal vector: {nW0}")
+    #     nw= nW0/np.linalg.norm(nW0)
+
+    #     # Distance from center (0,0,0) to wall plane
+    #     distance1 = abs(np.dot(nw, wA))
+    #     distance2 = abs(np.dot(nw, wB))
+    #     distance3 = abs(np.dot(nw, wC))
+    #     distance = np.mean([distance1, distance2, distance3])
+    #     self.get_logger().info(f"Distance to wall from center: {distance:.2f} cm")
+
+    #     # Pitch and yaw error angles 
+    #     yaw = np.arctan2(nw[0],nw[2])
+    #     pitch = np.arctan2(nw[1],nw[2])
+    #     yaw_deg = np.rad2deg(yaw)
+    #     pitch_deg = np.rad2deg(pitch)
+
+    #     self.get_logger().info(f"Pitch: {pitch_deg:.2f} degrees")
+    #     self.get_logger().info(f"Yaw: {yaw_deg:.2f} degrees")
+
+    #     rot = self.end_effector_pose.orientation
+    #     euler = R.from_quat([rot.x, rot.y, rot.z, rot.w]).as_euler('xyz')
+    #     self.get_logger().info(f"Current Orientation (rpy): roll={euler[0]:.2f}, pitch={euler[1]:.2f}, yaw={euler[2]:.2f}")
+    #     goal_orn = [euler[0], euler[1] + pitch, euler[2] + yaw]
+    #     self.get_logger().info(f"Demanded Orientation (rpy): roll={goal_orn[0]:.2f}, pitch={goal_orn[1]:.2f}, yaw={goal_orn[2]:.2f}")
+    #     goal_orn = R.from_euler('xyz', goal_orn, degrees=False)
+    #     q = goal_orn.as_quat()
+
+    #     # Convert orientation quaternion to rotation matrix
+    #     rot = self.end_effector_pose.orientation
+    #     r = R.from_quat([rot.x, rot.y, rot.z, rot.w])
+    #     nw_global = r.apply(nw)
+    #     q = r.apply([0, pitch, yaw])
+    #     q = R.from_euler('xyz', q, degrees=False)
+    #     q = q.as_quat()
+
+    #     # Current end effector position
+    #     pos = self.end_effector_pose.position
+    #     p_current = np.array([pos.x, pos.y, pos.z])
+
+    #     # Compute corrected position
+    #     self.get_logger().warn(f"Toggle Value: {self.toggle}")
+    #     p_new = p_current - 0*(self.toggle)*abs(distance - self.ideal_distance) * nw_global / 100     # in meters!!
+    #     self.toggle *= -1
+
+    #     # Rotation matrix and transform matrix
+    #     T = np.eye(4)
+    #     T[:3, :3] = R.from_quat(q).as_matrix()
+    #     T[:3, 3] = p_new
+
+    #     q_current = np.array([self.current_joint_state.position[-1], self.current_joint_state.position[0], self.current_joint_state.position[1], self.current_joint_state.position[2], self.current_joint_state.position[3], self.current_joint_state.position[4]])
+    #     joint_values = closed_form_algorithm(T, q_current, type=0)
+    #     if np.any(np.isnan(joint_values)):
+    #         self.get_logger().error("IK solution contains NaN. Aborting.")
+    #         return
+
+    #     # Publish GoalPose
+    #     traj_msg = JointTrajectory()
+    #     traj_msg.joint_names = [
+    #         'shoulder_pan_joint',
+    #         'shoulder_lift_joint',
+    #         'elbow_joint',
+    #         'wrist_1_joint',
+    #         'wrist_2_joint',
+    #         'wrist_3_joint'
+    #     ]
+    #     time_from_start = 0.5
+    #     goal_pose = JointTrajectoryPoint()
+    #     goal_pose.positions = joint_values.tolist()
+    #     goal_pose.time_from_start.sec = int(time_from_start)
+    #     goal_pose.time_from_start.nanosec = int((time_from_start % 1.0) * 1e9)
+    #     traj_msg.points.append(goal_pose)
+    #     self.trajectory_pub.publish(traj_msg)
+
+    #     # goal_pose = Pose()
+    #     # goal_pose.position.x = p_new[0]
+    #     # goal_pose.position.y = p_new[1]
+    #     # goal_pose.position.z = p_new[2]
+    #     # goal_pose.orientation.x = q[0]
+    #     # goal_pose.orientation.y = q[1]
+    #     # goal_pose.orientation.z = q[2]
+    #     # goal_pose.orientation.w = q[3]
+    #     # self.publisher_goal_pose.publish(goal_pose)
 
 def main(args=None):
     rclpy.init(args=args)
